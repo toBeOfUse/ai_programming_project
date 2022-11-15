100d9474e7f32ed9f6f6a94523bd0f4a499a3d83
toBeOfUse <mitchjacov@gmail.com>
2022-11-15 13:11:32 -0500

    regression: add line equation output

--- linear_regression/regression.py
+++ linear_regression/regression.py
@@ -5,6 +5,9 @@ class Line:
         self.alpha = alpha
         self.beta = beta
     
+    def __repr__(self):
+        return f"y = {self.beta}x + {self.alpha}"
+    
     def get_y(self, x: float) -> float:
         return self.beta*x+self.alpha
     
@@ -29,18 +32,18 @@ if __name__ == "__main__":
         print("filename argument for data required")
     else:
         with open(sys.argv[1]) as datafile:
-            points = []
-            for point in datafile:
-                points.append(tuple(map(float, point.split(","))))
-            line = Line.regression_from_points(points)
-            user_input = ""
-            while True:
-                user_input = input("Enter an x to receive the estimated y, or enter \"stop\" to stop: ")
-                if user_input == "stop":
-                    break
-                else:
-                    try:
-                        x = float(user_input)
-                        print("y: ", line.get_y(x))
-                    except:
-                        print("invalid input :(")
+            points = [tuple(map(float, point.split(","))) for point in datafile]
+        line = Line.regression_from_points(points)
+        print(f"Read {len(points)} points from {sys.argv[1]}")
+        print(f"Linear regression gave the line {line}")
+        user_input = ""
+        while True:
+            user_input = input("Enter an x to receive the predicted y, or enter \"stop\" to stop: ")
+            if user_input == "stop":
+                break
+            else:
+                try:
+                    x = float(user_input)
+                    print("y:", line.get_y(x))
+                except:
+                    print("invalid input :(")

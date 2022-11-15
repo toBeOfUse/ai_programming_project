add15c1f378df6c2c28bc2bcd603d08472d75ca1
toBeOfUse <mitchjacov@gmail.com>
2022-11-15 13:24:54 -0500

    call points file "points"

--- linear_regression/regression.py
+++ linear_regression/regression.py
@@ -29,7 +29,7 @@ class Line:
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("filename argument for data required")
+        print("filename argument for points required")
     else:
         with open(sys.argv[1]) as datafile:
             points = [tuple(map(float, point.split(","))) for point in datafile]

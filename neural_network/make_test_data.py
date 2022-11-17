cc26c7fb5e20e981ffe1cef3c7ba836e04d89b8a
toBeOfUse <mitchjacov@gmail.com>
2022-11-17 09:07:49 -0500

    let x and y be negative in test data

--- neural_network/make_test_data.py
+++ neural_network/make_test_data.py
@@ -1,8 +1,8 @@
 import math
-from random import random
+from random import uniform
 
 with open("atan2.csv", "w+") as data_output:
     for _ in range(0, 100):
-        x = random()*100
-        y = random()*100-50
+        x = uniform(-50, 50)
+        y = uniform(-50, 50)
         data_output.write(f"{x},{y},{math.atan2(y,x)}\n")

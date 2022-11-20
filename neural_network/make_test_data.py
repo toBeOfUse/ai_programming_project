30ae9c430ed7047bcf4de88d3635de748e56d3a1
toBeOfUse <mitchjacov@gmail.com>
2022-11-20 18:20:30 -0500

    adjust parameters aimlessly

--- neural_network/make_test_data.py
+++ neural_network/make_test_data.py
@@ -1,8 +1,12 @@
 import math
 from random import uniform
 
-with open("atan2.csv", "w+") as data_output:
-    for _ in range(0, 1000):
-        x = uniform(-50, 50)
-        y = uniform(-50, 50)
-        data_output.write(f"{x},{y},{math.atan2(y,x)}\n")
+def atan2():
+    with open("atan2.csv", "w+") as data_output:
+        for _ in range(0, 10000):
+            x = uniform(-10, 10)
+            y = uniform(-10, 10)
+            data_output.write(f"{x},{y},{math.atan2(y,x)}\n")
+
+if __name__ == "__main__":
+    atan2()

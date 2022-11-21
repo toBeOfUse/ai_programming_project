e5c5a30d0b6ab866299c43bd8bffd3c296f6323a
toBeOfUse <mitchjacov@gmail.com>
2022-11-20 21:10:13 -0500

    reduce noise in regression data

--- linear_regression/generate_many_points.py
+++ linear_regression/generate_many_points.py
@@ -2,4 +2,4 @@ from random import uniform
 x = [uniform(-100, 100) for _ in range(100)]
 with open("points.csv", "w+") as points_file:
     for x_ in x:
-        points_file.write(f"{x_},{(2*x_+5)+uniform(-50,50)}\n")
+        points_file.write(f"{x_},{(2*x_+5)+uniform(-10,10)}\n")

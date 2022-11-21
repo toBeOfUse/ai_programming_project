2ac1cfb16ea3a3b59f3fe457355b04d3a0695b1f
toBeOfUse <mitchjacov@gmail.com>
2022-11-20 22:41:54 -0500

    move annotations line

--- linear_regression/regression.py
+++ linear_regression/regression.py
@@ -1,7 +1,7 @@
+from __future__ import annotations
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
-from __future__ import annotations
 
 class Line:
     def __init__(self, alpha, beta):
